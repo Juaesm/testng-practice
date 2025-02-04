0ee26ea62554ce87b547a6dd351eff2a0f101828
Juan Muñoz <juan.munoz02@globant.com>
2025-02-04 11:38:53 -0500

    Feat: Search and languague change

--- testng-practice/src/test/java/com/globant/testng/wikipediaTest.java
+++ testng-practice/src/test/java/com/globant/testng/wikipediaTest.java
@@ -26,50 +26,67 @@ public class wikipediaTest{
         driver.manage().window().maximize();
     }
 
-    @BeforeMethod
+    @BeforeClass(dependsOnMethods = "testSetup")
     public void openBrowser()
     {
         driver.get("https://en.wikipedia.org/wiki/Main_Page");
-        driver.findElement(By.id("pt-createaccount-2")).click();
+        driver.findElement(By.id("pt-login-2")).click();
         System.out.println("We are currently on the following URL" + driver.getCurrentUrl());
-    }
+    }   
 
-    @BeforeMethod(description = "This method validates the signup functionality")
+    /*@BeforeMethod(description = "This method validates the signup functionality")
     public void signup() throws InterruptedException
     {
 
         System.out.println("In SignUp Window");
-        driver.findElement(By.id("wpName2")).sendKeys("testng_jm_1");
+        driver.findElement(By.id("wpName2")).sendKeys("testng_jm_3");
         driver.findElement(By.id("wpPassword2")).sendKeys("Pass1234*");
         driver.findElement(By.id("wpRetype")).sendKeys("Pass1234*");
-
-        driver.wait(10000);
 
         driver.findElement(By.id("wpCreateaccount")).click();
 
         System.out.println("Registration successful");
-
-
-
-    }
+    }*/
 
     @Test(description = "This method validates the login functionality")
     public void login()
     {
 
-        driver.get("https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page");
-
-        driver.findElement(By.id("wpName1")).sendKeys("test1000");
+        driver.findElement(By.id("wpName1")).sendKeys("Testng jm 3");
         driver.findElement(By.id("wpPassword1")).sendKeys("Pass1234*");
 
         driver.findElement(By.id("wpLoginAttempt")).click();
-        
+
+
         System.out.println("Login successful");
 
-
-
-
     }
+
+    @Test(dependsOnMethods = "login", description = "This method validates the log out functionality")
+    public void logout()
+    {
+
+        driver.findElement(By.id("vector-user-links-dropdown-checkbox")).click();
+        
+        driver.findElement(By.id("pt-logout")).click();
+
+
+        System.out.println("Logout Completed");
+    }
+
+    @Test(description = "This method validates a search and change the language")
+    public void testSearch()
+    {
+        driver.get("https://en.wikipedia.org/wiki/George_W._Bush");
+        System.out.println("Article Find");
+        driver.findElement(By.id("p-lang-btn-checkbox")).click();
+        driver.findElement(By.className("interlanguage-link interwiki-fr mw-list-item")).click();
+        System.out.println("Language Changed");
+    }
+    
+    
+    
+    
     @AfterClass
     public void afterClass()
     {
